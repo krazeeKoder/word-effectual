a2cb0c494c13d618aa59f630f0464322b3036f3e
KrazeeKoder <tony.tulai@gmail.com>
2016-01-11 16:32:25 -0500

    Initial Commit

---
+++ AssignmentJan11/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AssignmentJan11
+//
+//  Created by Anthony Tulai on 2016-01-11.
+//  Copyright © 2016 Anthony Tulai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
